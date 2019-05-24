1aeb89c6ec8381af3f3711188ab39e640f7c4251
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-24 17:30:29 -0400

    Add React Client

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -1,16 +1,22 @@
 pragma solidity ^0.5.0;
 
+import "zos-lib/contracts/Initializable.sol";
 
-contract ChatApp {
+contract ChatApp is Initializable {
     
     mapping(uint => string) public idToRoom;
     mapping(uint => bool) public roomExists;
     mapping(bytes32 => uint) public roomRecord;
     
-    uint roomCount = 1;
+    uint public roomCount;
     
     event message(string message,  uint roomId, string roomName, address user);
     event roomAdded(uint roomId, string room, address user);
+
+    function initialize() initializer public{
+        roomCount = 1;
+
+    }
     
     
     function makeRoom(string memory _roomName) public returns(uint256){

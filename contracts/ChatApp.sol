3ca76b17d028fe6ace9e65304232616f393de9e8
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-24 15:54:50 -0400

    First commit + Solidity contract

---
+++ contracts/ChatApp.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+
+contract ChatApp {
+    
+    mapping(uint => string) public idToRoom;
+    mapping(uint => bool) public roomExists;
+    mapping(bytes32 => uint) public roomRecord;
+    
+    uint roomCount = 1;
+    
+    event message(string message,  uint roomId, string roomName, address user);
+    event roomAdded(uint roomId, string room, address user);
+    
+    
+    function makeRoom(string memory _roomName) public returns(uint256){
+        require(roomRecord[keccak256(bytes(_roomName))] == 0, "The room already exists");
+        uint roomId = roomCount;
+        roomRecord[keccak256(bytes(_roomName))] =  roomId;
+        idToRoom[roomId] = _roomName;
+        roomExists[roomId] = true;
+        emit roomAdded(roomId, idToRoom[roomId], msg.sender);
+        roomCount++;
+        return roomId;
+        
+    }
+    
+    function makeMsg(string memory _msg, uint256 _roomId) public {
+        require(roomExists[_roomId] == true, "The room does not exist");
+        emit message(_msg, _roomId,  idToRoom[_roomId], msg.sender);
+    }
+    
+    
+}
